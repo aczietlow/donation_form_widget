1d905314242b93c0befe75bf812ec67935648490
Chris Zietlow <aczietlow@spyderbytedesign.com>
2013-03-24 12:48:21 -0400

    fixed updateSLider function

--- formWidget.js
+++ formWidget.js
@@ -3,16 +3,24 @@ $(document).ready(function(){
 	$('.slider').slider({
 		min: 0,
 		max: 100,
-		value: 10,
+		value: 50,
 		slide: function(event, ui) {
-			$('.amount').val( ui.value );
-			updateSlider( ui.value );
+			var idName = $(this).attr('id');
+			$('.' + idName).val( ui.value );
+			updateSlider( ui.value, idName );
 		}
 			});
-	$('.amount').val( $('.slider').slider('value') );
+	$('.alug').val( $('.slider').slider('value') );
+	$('.hack-augusta').val( $('.slider').slider('value') );
 	
 	
-	function updateSlider(value) {
+	function updateSlider(value, idName) {
 		$('.test').html( (value/100) * money );
+		var averageValue = 100 - value;
+		if (idName == 'alug') {
+			var value = $('#hack-augusta').slider('value');
+			$('#hack-augusta').slider('value', averageValue);
+		}
+		
 	}
 });
